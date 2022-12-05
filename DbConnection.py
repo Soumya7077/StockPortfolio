2201e9cae0eab67a8dcc3ac26f6f36f266a25f75
Soumya7077 <110816458+Soumya7077@users.noreply.github.com>
2022-12-05 12:05:10 +0530

    commit from ide

--- DbConnection.py
+++ DbConnection.py
@@ -4,6 +4,7 @@ from InsertRecord import *
 from StockDetailsCsvRead import *
 from SectorMasterFetch import *
 from Categorymatser import categorymaster, subcategorymaster, stocksmaster
+from FetchByJoin import  *
 import sys
 
 menu()
@@ -12,8 +13,8 @@ while (True):
     try:
         print("-" * 50)
         ch = int(input("Enter your choice:"))
-        if (ch <= 0 or ch > 10):
-            print("Please Enter a number between 1 to 10")
+        if (ch <= 0 or ch > 11):
+            print("Please Enter a number between 1 to 11")
         else:
             print("-" * 50)
             match (ch):
@@ -36,11 +37,13 @@ while (True):
                 case 9:
                     sectorMasterFetch()
                 case 10:
+                    fetchByJoin()
+                case 11:
                     con = input("Do you want to EXIT (yes/no):").lower()
                     print("-" * 50)
                     if (con == "yes"):
                         print("Thanks for using this program")
                         sys.exit()
     except ValueError as v:
-        print("Please enter number 1 to 10. Don't Enter strs/symbols/alnums")
+        print("Please enter number 1 to 11. Don't Enter strs/symbols/alnums")
         print(v)
--- FetchByJoin.py
+++ FetchByJoin.py
@@ -1,7 +1,7 @@
 import cx_Oracle
 
 
-def dbconnect():
+def fetchByJoin():
     con = cx_Oracle.connect("c##babul/soumya@localhost/orcl")
     cur = con.cursor()
     cur.execute("select s1.name as sector_name,s2.name as subsector_name,s1.id as parent_id,s2.id as id,sm.stock_code as code,sm.stock_name as stock_name from sectormaster s1 inner join sectormaster s2 on s1.id=s2.parentid inner join stockmaster sm on s2.id = sm.subcategory_id")
@@ -21,4 +21,4 @@ def dbconnect():
                                                                                    sectorName))
 
 
-dbconnect()
+
